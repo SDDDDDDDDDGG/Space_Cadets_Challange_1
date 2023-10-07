a38a6ed3eee07ae9566c63d55ccdd76ce11a54ca
Chenrui Yuan <chenrui.yuan2003@outlook.com>
2023-10-07 12:52:10 +0100

    Added code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        String url;
+
+        BufferedReader myBuffer = new BufferedReader();
+        url = "https://www.southampton.ac.uk/people/" + myBuffer.getEmailId();
+
+
+    }
+
+}
